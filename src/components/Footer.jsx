f30946663420c7fa7ecfdb6bc518130191de5acd
unknown <mhalder9555@gmail.com>
2023-12-05 21:11:52 -0600

    Added a footer and some styling for the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,22 @@
-
-
+import githubLogo from "../assets/images/github_icon.png";
+import gmailLogo from "../assets/images/gmail_icon.png";
 
 export default function Footer() {
 
   return (
-    <p>Copyright 2023 Matthew Halder. All rights reserved.</p>
+    <footer>
+      <section id="find-me" className="contact">
+        <h3>Find Me</h3>
+        <div>
+          <section id="github_icon">
+            <a href="https://github.com/mhalder4"><img src={githubLogo} alt="Github icon" /></a>
+          </section>
+          <section id="gmail_icon">
+            <a href="mailto:mhalder9555@gmail.com"><img src={gmailLogo} alt="Gmail icon" /></a>
+          </section>
+        </div>
+      </section>
+      &copy; 2023 Matthew Halder
+    </footer>
   )
 }
