7e365b7f029d5809ab4054bda1be3d59b3bc9330
unknown <mhalder9555@gmail.com>
2023-12-05 21:01:14 -0600

    Added some custom css from a testing folder that was in html and css only.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,21 @@ export default function Header(props) {
 
   return (
     <header>
-      <nav class="container-fluid row">
-        <div class="col-6">
-          <h2 class="name">Matt <span class="last">Halder</span></h2>
+      <nav className="container-fluid row">
+        <div className="col-6">
+          <h2 className="name">Matt <span className="last">Halder</span></h2>
         </div>
-        <div class="col-6 d-flex justify-content-around align-items-center">
-          <button class="button">
+        <div className="col-6 d-flex justify-content-around align-items-center">
+          <button className="button">
             Portfolio
           </button>
-          <button class="button">
+          <button className="button">
             About Me
           </button>
-          <button class="button">
+          <button className="button">
             Contact Me
           </button>
-          <button class="button">
+          <button className="button">
             Resume
           </button>
         </div>
